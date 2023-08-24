741611f78686f325ea59d364f34e4e5495a539ac
rahulrathore2110 <rahulg27727@gmail.com>
2023-08-24 23:04:11 +0530

    addition

--- assets/scripts/otp.js
+++ assets/scripts/otp.js
@@ -48,7 +48,7 @@ function otpapi(otpnum, phone) {
   };
 
   fetch(
-    `http://sms.xcelmarketing.in/api/SmsApi/SendSingleApi?UserID=XCEL&Password=Om@nlum5749NL&SenderID=XCLSMS&Phno=${phone}&Msg=Thank you for showing your interest in Xcel Marketing. your mobile verification OTP is ${otpnum}. Do not share OTP with anyone.
+    `https://sms.xcelmarketing.in/api/SmsApi/SendSingleApi?UserID=XCEL&Password=Om@nlum5749NL&SenderID=XCLSMS&Phno=${phone}&Msg=Thank you for showing your interest in Xcel Marketing. your mobile verification OTP is ${otpnum}. Do not share OTP with anyone.
 &EntityID=1701159834356736157&TemplateID=1707168965160700422`,
     requestOptions
   )
